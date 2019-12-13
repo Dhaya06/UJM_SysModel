3988bd77d456efdb3d423405b576f634b9021913
Dhaya Raster <gster434@gmail.com>
2019-12-13 18:39:46 +0100

    fixes in OOP to json

---
+++ Json/JsonToOOP_Source/ClassGenerator.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Xamasoft.JsonClassGenerator.UI
+{
+    partial class ClassGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkOpenFolder = new Xamasoft.Controls.BetterLinkLabel();
+            this.lblDoneClipboard = new System.Windows.Forms.Label();
+            this.edtJson = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edtMainClass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.edtTargetFolder = new System.Windows.Forms.TextBox();
+            this.lblNamespace = new System.Windows.Forms.Label();
+            this.edtNamespace = new System.Windows.Forms.TextBox();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lnkOpenFolder
+            // 
+            this.lnkOpenFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnkOpenFolder.AutoSize = true;
+            this.lnkOpenFolder.Location = new System.Drawing.Point(470, 436);
+            this.lnkOpenFolder.Name = "lnkOpenFolder";
+            this.lnkOpenFolder.Size = new System.Drawing.Size(62, 13);
+            this.lnkOpenFolder.TabIndex = 43;
+            this.lnkOpenFolder.TabStop = true;
+            this.lnkOpenFolder.Text = "Open folder";
+            this.lnkOpenFolder.Visible = false;
+            this.lnkOpenFolder.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkOpenFolder_LinkClicked);
+            // 
+            // lblDoneClipboard
+            // 
+            this.lblDoneClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDoneClipboard.AutoSize = true;
+            this.lblDoneClipboard.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDoneClipboard.ForeColor = System.Drawing.Color.Green;
+            this.lblDoneClipboard.Location = new System.Drawing.Point(281, 436);
+            this.lblDoneClipboard.Name = "lblDoneClipboard";
+            this.lblDoneClipboard.Size = new System.Drawing.Size(183, 13);
+            this.lblDoneClipboard.TabIndex = 42;
+            this.lblDoneClipboard.Text = "Done! Classes copied to clipboard";
+            this.lblDoneClipboard.Visible = false;
+            // 
+            // edtJson
+            // 
+            this.edtJson.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edtJson.Location = new System.Drawing.Point(53, 155);
+            this.edtJson.MaxLength = 10000000;
+            this.edtJson.Multiline = true;
+            this.edtJson.Name = "edtJson";
+            this.edtJson.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.edtJson.Size = new System.Drawing.Size(680, 271);
+            this.edtJson.TabIndex = 40;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.Location = new System.Drawing.Point(577, 431);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 41;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Main class name:";
+            // 
+            // edtMainClass
+            // 
+            this.edtMainClass.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default, "MainClassName", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.edtMainClass.Location = new System.Drawing.Point(171, 40);
+            this.edtMainClass.Name = "edtMainClass";
+            this.edtMainClass.Size = new System.Drawing.Size(185, 20);
+            this.edtMainClass.TabIndex = 45;
+            this.edtMainClass.Text = global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default.MainClassName;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Target folder:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(332, 66);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(24, 23);
+            this.btnBrowse.TabIndex = 47;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // edtTargetFolder
+            // 
+            this.edtTargetFolder.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.edtTargetFolder.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories;
+            this.edtTargetFolder.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default, "TargetFolder", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.edtTargetFolder.Location = new System.Drawing.Point(171, 68);
+            this.edtTargetFolder.Name = "edtTargetFolder";
+            this.edtTargetFolder.Size = new System.Drawing.Size(155, 20);
+            this.edtTargetFolder.TabIndex = 46;
+            this.edtTargetFolder.Text = global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default.TargetFolder;
+            // 
+            // lblNamespace
+            // 
+            this.lblNamespace.AutoSize = true;
+            this.lblNamespace.Location = new System.Drawing.Point(68, 15);
+            this.lblNamespace.Name = "lblNamespace";
+            this.lblNamespace.Size = new System.Drawing.Size(67, 13);
+            this.lblNamespace.TabIndex = 48;
+            this.lblNamespace.Text = "Namespace:";
+            // 
+            // edtNamespace
+            // 
+            this.edtNamespace.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default, "Namespace", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.edtNamespace.Location = new System.Drawing.Point(171, 12);
+            this.edtNamespace.Name = "edtNamespace";
+            this.edtNamespace.Size = new System.Drawing.Size(185, 20);
+            this.edtNamespace.TabIndex = 44;
+            this.edtNamespace.Text = global::Xamasoft.JsonClassGenerator.UI.Properties.Settings.Default.Namespace;
+            // 
+            // lblDone
+            // 
+            this.lblDone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDone.AutoSize = true;
+            this.lblDone.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDone.ForeColor = System.Drawing.Color.Green;
+            this.lblDone.Location = new System.Drawing.Point(236, 436);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(39, 13);
+            this.lblDone.TabIndex = 51;
+            this.lblDone.Text = "Done!";
+            this.lblDone.Visible = false;
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.DisplayMember = "DisplayName";
+            this.cmbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Location = new System.Drawing.Point(193, 107);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(185, 21);
+            this.cmbLanguage.TabIndex = 52;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(644, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "By Dharmalingam Dhayananth";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 13);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Please past your json in text box and press Generate";
+            // 
+            // ClassGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 466);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edtMainClass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.edtTargetFolder);
+            this.Controls.Add(this.lblNamespace);
+            this.Controls.Add(this.edtNamespace);
+            this.Controls.Add(this.lnkOpenFolder);
+            this.Controls.Add(this.lblDoneClipboard);
+            this.Controls.Add(this.edtJson);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "ClassGenerator";
+            this.Text = "Class Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.BetterLinkLabel lnkOpenFolder;
+        private System.Windows.Forms.Label lblDoneClipboard;
+        private System.Windows.Forms.TextBox edtJson;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edtMainClass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox edtTargetFolder;
+        private System.Windows.Forms.Label lblNamespace;
+        private System.Windows.Forms.TextBox edtNamespace;
+        private System.Windows.Forms.Label lblDone;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
---
+++ Json/JsonToOOP_Source/ClassGenerator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Xamasoft.JsonClassGenerator.CodeWriters;
+using System.Diagnostics;
+
+
+namespace Xamasoft.JsonClassGenerator.UI
+{
+    public partial class ClassGenerator : Form
+    {
+        public ClassGenerator()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            initiate();
+            label1.Visible = true;
+            cmbLanguage.Visible = false;
+        }
+
+        private void HideCompletionMessage()
+        {
+
+            lnkOpenFolder.Visible = false;
+            lblDone.Visible = false;
+            lblDoneClipboard.Visible = false;
+        }
+        private JsonClassGenerator Prepare()
+        {
+            
+            var gen = new JsonClassGenerator();
+            gen.Example = edtJson.Text;
+            gen.InternalVisibility = true;
+            //gen.CodeWriter = (ICodeWriter)cmbLanguage.SelectedItem;
+            gen.CodeWriter = (ICodeWriter)cmbLanguage.Items[0];
+            //gen.ExplicitDeserialization = chkExplicitDeserialization.Checked && gen.CodeWriter is CSharpCodeWriter;
+            gen.Namespace = string.IsNullOrEmpty(edtNamespace.Text) ? null : edtNamespace.Text;
+            gen.NoHelperClass = true;
+            gen.SecondaryNamespace = "SystemModelingNameSpace";
+            //gen.TargetFolder = edtTargetFolder.Text;
+            gen.TargetFolder = desitinationPath;
+            gen.UseProperties = false;
+            gen.MainClass = edtMainClass.Text;
+            gen.UsePascalCase = false;
+            gen.UseNestedClasses = false;
+            gen.ApplyObfuscationAttributes = false;
+            gen.SingleFile = true;
+            gen.ExamplesInDocumentation = false;
+            return gen;
+        }
+
+
+        private void initiate()
+        {
+          
+
+            cmbLanguage.Items.AddRange(CodeWriters);
+
+
+        }
+
+        private string lastGeneratedString;
+
+        
+        private readonly static ICodeWriter[] CodeWriters = new ICodeWriter[] {
+            new CSharpCodeWriter()
+            
+        };
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (desitinationPath == "" || string.IsNullOrEmpty(desitinationPath))
+            {
+                MessageBox.Show("Please select a target folder to store your generated class file");
+                return;
+            }
+
+            HideCompletionMessage();
+            
+            var gen = Prepare();
+            if (gen == null) return;
+            try
+            {
+                gen.GenerateClasses();
+              
+                lblDone.Visible = true;
+                lnkOpenFolder.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Unable to generate the code: " + ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lnkOpenFolder_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Process.Start(desitinationPath);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        string desitinationPath = "";
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Select a location to store decoded image file");
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && fbd.SelectedPath !="")
+                {
+                    desitinationPath = fbd.SelectedPath;
+                }
+            }
+        }
+    }
+}
